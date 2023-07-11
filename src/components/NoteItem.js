ca8c99f22ad2413a8a17777dfb693e44489c1edf
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-07-11 11:46:33 +0530

    added tag as badge display

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -5,9 +5,28 @@ import noteContext from '../context/notes/noteContext';
 const NoteItem = (props) => {
   const { deleteNote } = useContext(noteContext);
   const { note, updateNote } = props;
+
+  let type = 'primary';
+  if (note.tag === 'personal') type = 'success';
+  else if (note.tag === 'work') type = 'warning';
+  else if (note.tag === 'important') type = 'danger';
+
   return (
     <div className="col-md-3 my-3">
       <Card>
+        <div className="mb-2">
+          <span
+            className={`badge rounded-pill bg-${type}`}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              position: 'absolute',
+              right: '0',
+            }}
+          >
+            {note.tag}
+          </span>
+        </div>
         <Card.Body>
           <div className="d-flex justify-content-end">
             <Card.Title className="flex-grow-1">{note.title}</Card.Title>
